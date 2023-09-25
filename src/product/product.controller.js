3b681a618eac43a672c6816590ffccd5feae05c2
Dicky <muhammad.dickynn@gmail.com>
2023-09-25 20:39:23 +0700

    product (GET,PUT,PATCH,DELETE) done

--- src/product/product.controller.js
+++ src/product/product.controller.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const prisma = require("../db");
-const { getAllProducts, postProductById, deleteProductById } = require("./product.service");
+const { getAllProducts, postProductById, deleteProductById, getProductById, patchProductById } = require("./product.service");
 
 
 
 const router = express.Router();
 
-
+// GET product
 router.get("/", async (req, res) => {
     const product = await getAllProducts();
     res.send(product);
 });
 
+// GET product by ID
+router.get("/:id", async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const productResponse = await getProductById(parseInt(productId));
+        res.send(productResponse);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+})
 
-
-
-
+// POST product
 router.post("/", async (req, res) => {
     const productData = req.body;
     const productResponse = await postProductById(productData);
@@ -25,6 +33,18 @@ router.post("/", async (req, res) => {
     })
 })
 
+// PATCH product by ID
+router.patch("/:id", async (req, res) => {
+    const productId = req.params.id;
+    const productData = req.body;
+    const productResponse = await patchProductById(parseInt(productId), productData)
+    res.send({
+        data: productResponse,
+        message: "Produk berhasil di update"
+    })
+})
+
+// DELETE product by ID
 router.delete("/:id", async (req, res) => {
     try {
         const productId = req.params.id;
--- src/product/product.service.js
+++ src/product/product.service.js
@@ -1,11 +1,28 @@
 const prisma = require("../db");
 
+// GET all product
 const getAllProducts = async () => {
     const products = await prisma.product.findMany();
     return products;
 }
 
+// GET product by ID
+const getProductById = async (id) => {
+    if (typeof id !== "number") {
+        throw Error("ID harus number")
+    }
+    const productResponse = await prisma.product.findUnique({
+        where: {
+            id: parseInt(id),
+        }
+    })
+    if (!productResponse) {
+        throw Error("Data tidak ada")
+    }
+    return productResponse;
+}
 
+// POST product
 const postProductById = async (productData) => {
     const product = await prisma.product.create({
         data: {
@@ -18,6 +35,34 @@ const postProductById = async (productData) => {
     return product;
 }
 
+const patchProductById = async (id, productData) => {
+    if (typeof id !== "number") {
+        throw Error("ID harus number")
+    }
+    const productResponse = await prisma.product.findUnique({
+        where: {
+            id: parseInt(id),
+        }
+    })
+    if (!productResponse) {
+        throw Error("Data tidak ada")
+    }
+    await prisma.product.update({
+        where: {
+            id: parseInt(id),
+        },
+        data: {
+            name: productData.name,
+            price: productData.price,
+            description: productData.description,
+            image: productData.image
+        }
+    })
+    return productResponse
+
+}
+
+// DELETE product by ID
 const deleteProductById = async (id) => {
     if (typeof id !== "number") {
         throw Error("Id harus number");
@@ -41,4 +86,6 @@ module.exports = {
     getAllProducts,
     postProductById,
     deleteProductById,
+    getProductById,
+    patchProductById
 }
